e27efb3cb5a1d62f5991e9cadb0fd9ce4dc518aa
Sarah Iraurgui <iraurguisj@gmail.com>
2020-09-23 23:23:56 -0400

    card css

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ var formText = document.querySelector("#input-form");
 
 var formBtn = document.querySelector("#btn-form");
 
-
+//empty array of cities
 var citiesArr = [];
 
 
@@ -22,11 +22,12 @@ $(document).ready(function () {
         //get the city that the user put in 
         var cityInput = $("#weather-input").val().trim();
 
-      
+
         //get current weather with the city they chose
         currentWeather(cityInput)
         //get the 5 day forecast with the city they chose
         forecast(cityInput)
+
 
     })
 
@@ -46,45 +47,53 @@ $(document).ready(function () {
             $(".temperature").text("Temperature " + data.main.temp);
             $(".humidity").text("Humidity " + data.main.humidity);
             $(".windspeed").text("Windspeed " + data.wind.speed);
-
+            //call uv function
             uv(lat, long);
         })
     }
 
 
-
+    //function for getting and displaying 5 day forecast
     function forecast(city) {
 
 
 
-
+        //make an ajax call to get the 5day forecast          
         $.ajax({
             method: "GET",
             url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=8f63f215819badad4ad86bbbab862826&units=imperial"
 
         }).then(function (data) {
-     
+
             //loop over data and create cards with info for each day
-            for (var i = 0; i < data.list.length; i+=8) {
+            for (var i = 0; i < data.list.length; i += 8) {
 
-              
-               $( ".forecast" ).append( "<div class='card'>Temp: " + data.list[i].main.temp + "</div>" );
-            var card = $("<div>");
-            var cardBody = $("<div>");
-            var cardText = $("<p>");
-            card.addClass("cardB");
-            cardBody.addClass("card-body");
-            cardText.text(data.list[i].main.temp);
-            cardBody.append(cardText);
-            card.append(cardBody);
-            $(".forecast").append(card);
+
+                var card = $("<div>").addClass("card");
+
+
+                var card = $("<div>").addClass("card");
+
+
+                var card = $("<div>").addClass("card");
+
+                var card = $("<div>").addClass("card");
+
+                var card = $("<div>").addClass("card");
+
+
+
+                //make temp text appear on cards
+                $(".forecast").append("<div class='cardB'>Temp: " + data.list[i].main.temp + "</div>");
+
+
+                //make humidity appear on cards
+                //    $( ".forecast" ).append( "<div class='cardB'>Humidity: " + data.main.humidity + "</div>" );
+
+
+            }
+
             
-
-              }
-            //append each card to the forecast div
-        
-
-{}
         })
 
 
